36bd24bd154cd16281b4062e682abfff5299697e
SteveOC64 <steveoc64@gmail.com>
2016-01-17 23:47:49 +1030

    add some MD superstylin

--- chapter2/demo.go
+++ chapter2/demo.go
@@ -12,6 +12,16 @@ func inputKeyUp(event dom.Event) {
 	span.SetInnerHTML(input.Value)
 }
 
+func checkParent(target, elm dom.Element) bool {
+	for target.ParentElement() != nil {
+		if target.IsEqualNode(elm) {
+			return true
+		}
+		target = target.ParentElement()
+	}
+	return false
+}
+
 func main() {
 	w := dom.GetWindow()
 	d := w.Document()
@@ -19,6 +29,8 @@ func main() {
 	k := d.GetElementByID("keycode")
 	i := d.GetElementByID("input").(*dom.HTMLInputElement)
 	vv := d.GetElementByID("vv").(*dom.HTMLDivElement)
+	hidex := d.GetElementByID("hidex").(*dom.HTMLDivElement)
+	createx := d.GetElementByID("createx").(*dom.HTMLDivElement)
 
 	print(vv.Dataset()["what"])
 	h.AddEventListener("click", false, func(event dom.Event) {
@@ -35,4 +47,42 @@ func main() {
 	i.Focus()
 	i.AddEventListener("keyup", false, inputKeyUp)
 
+	hidex.AddEventListener("click", false, func(event dom.Event) {
+		hidex.Style().SetProperty("display", "none", "")
+	})
+
+	createx.AddEventListener("click", false, func(event dom.Event) {
+		div := d.CreateElement("div").(*dom.HTMLDivElement)
+		div.Style().SetProperty("color", "red", "")
+		div.SetTextContent("I am new div")
+		createx.AppendChild(div)
+	})
+
+	audbtn := d.GetElementByID("audbtn").(*dom.HTMLButtonElement)
+	audbtn.AddEventListener("click", false, func(event dom.Event) {
+		a := d.GetElementByID("audio").(*dom.HTMLAudioElement)
+		if a.Paused {
+			a.Play()
+			audbtn.SetTextContent("Click Me to Pause Sound")
+		} else {
+			a.Pause()
+			audbtn.SetTextContent("Click Me to Play Sound")
+		}
+	})
+
+	toggle := d.GetElementByID("menu-dropdown").(*dom.HTMLDivElement)
+	menu := d.GetElementByID("menuDiv-dropdown").(*dom.HTMLDivElement)
+
+	d.AddEventListener("click", false, func(event dom.Event) {
+		if !checkParent(event.Target(), menu) {
+			// click NOT on the menu
+			if checkParent(event.Target(), toggle) {
+				// click on the link
+				menu.Class().Toggle("invisible")
+			} else {
+				// click both outside link and outside menu, hide menu
+				menu.Class().Add("invisible")
+			}
+		}
+	})
 }
